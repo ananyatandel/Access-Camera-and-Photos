41cb0e319d7a0c1262833bcdadc20629ccd772f7
Ananya <ananyatandel02@gmail.com>
2020-07-14 11:42:25 -0600

    Initial Commit

---
+++ Access Camera and Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Access Camera and Photos
+//
+//  Created by Ananya Tandel on 7/14/20.
+//  Copyright © 2020 Ananya Tandel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
